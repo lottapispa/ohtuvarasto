39cf7df9f3b8c8f771b507249bd489f5d05d32f3
lottapispa <lotta.pispa@helsinki.fi>
2024-11-04 17:59:55 +0200

    testit valmiit

--- src/tests/varasto_test.py
+++ src/tests/varasto_test.py
@@ -10,17 +10,24 @@ class TestVarasto(unittest.TestCase):
         # https://docs.python.org/3/library/unittest.html#unittest.TestCase.assertAlmostEqual
         self.assertAlmostEqual(self.varasto.saldo, 0)
 
-    def test_uudella_varastolla_oikea_tilavuus(self):
+    def test_uudella_varastolla_oikea_tilavuus_ja_saldo(self):
         self.assertAlmostEqual(self.varasto.tilavuus, 10)
+        self.assertEqual(self.varasto.saldo, 0)
+
+    def test_uudella_varastolla_liian_pieni_tilavuus(self):
+        self.varasto = Varasto(-1)
+        self.assertAlmostEqual(self.varasto.tilavuus, 0)
+
+    def test_uudella_varastolla_vaara_saldo(self):
+        self.varasto = Varasto(10, -1)
+        self.assertAlmostEqual(self.varasto.saldo, 0)
 
     def test_lisays_lisaa_saldoa(self):
         self.varasto.lisaa_varastoon(8)
-
         self.assertAlmostEqual(self.varasto.saldo, 8)
 
     def test_lisays_lisaa_pienentaa_vapaata_tilaa(self):
         self.varasto.lisaa_varastoon(8)
-
         # vapaata tilaa pitäisi vielä olla tilavuus-lisättävä määrä eli 2
         self.assertAlmostEqual(self.varasto.paljonko_mahtuu(), 2)
 
